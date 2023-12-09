a00e53d5f88a69530cbf2c15e0ef077ee6c52e24
jupiter <pengjupiter5@gmail.com>
2023-12-09 16:02:09 -0600

    add margin in preparation for snapping away to prevent overlapping

--- src/main/java/redilog/init/RedilogComponents.java
+++ src/main/java/redilog/init/RedilogComponents.java
@@ -1,23 +1,13 @@
 package redilog.init;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.minecraft.block.BlockState;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtHelper;
-import net.minecraft.nbt.NbtIntArray;
 import net.minecraft.nbt.NbtIo;
-import net.minecraft.nbt.NbtList;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Vec3i;
-import redilog.routing.BLOCK;
 import redilog.synthesis.nodes.Component;
-import redilog.utils.Vec4i;
 
 public class RedilogComponents {
     public static Component OR_GATE;
@@ -35,82 +25,10 @@ public class RedilogComponents {
     private static Component loadComponent(ResourceManager rm, Identifier id) {
         try {
             NbtCompound nbt = NbtIo.readCompressed(rm.open(id));
-            Component c = new Component(readv3i(nbt.get("size")));
-
-            for (NbtElement e : nbt.getList("inputs", NbtElement.INT_ARRAY_TYPE)) {
-                c.inputs.add(readv3i(e));
-            }
-            if (c.inputs.size() == 0) {
-                Redilog.LOGGER.warn(String.format("%s has 0 inputs", id));
-            }
-
-            for (NbtElement e : nbt.getList("outputs", NbtElement.INT_ARRAY_TYPE)) {
-                c.outputs.add(readv4i(e));
-            }
-            if (c.outputs.size() == 0) {
-                Redilog.LOGGER.warn(String.format("%s has 0 outputs", id));
-            }
-
-            List<BlockState> states = new ArrayList<>();
-            for (NbtElement e : nbt.getList("palette", NbtElement.COMPOUND_TYPE)) {
-                states.add(NbtHelper.toBlockState((NbtCompound) e));
-            }
-            boolean nonAir = false;
-            for (NbtElement e : nbt.getList("blocks", NbtElement.COMPOUND_TYPE)) {
-                NbtCompound e2 = (NbtCompound) e;
-                BlockState state = states.get(e2.getInt("state"));
-                c.blocks.set(readv3i(e2.get("pos")), BLOCK.fromState(state));
-                if (!state.isAir()) {
-                    nonAir = true;
-                }
-            }
-            if (!nonAir) {
-                Redilog.LOGGER.warn(String.format("%s has only air blocks", id));
-            }
-            return c;
+            return new Component(nbt, id);
         } catch (IOException | AssertionError e) {
             Redilog.LOGGER.error(String.format("Unable to load %s", id), e);
+            return null;
         }
-        return null;
-    }
-
-    private static Vec3i readv3i(NbtElement nbt) {
-        int[] ar = null;
-        if (nbt instanceof NbtIntArray nbtia) {
-            ar = nbtia.getIntArray();
-        }
-        if (nbt instanceof NbtList nbtl) {
-            ar = new int[nbtl.size()];
-            for (int i = 0; i < nbtl.size(); ++i) {
-                ar[i] = nbtl.getInt(i);
-            }
-        }
-        if (ar == null) {
-            throw new AssertionError(String.format("%s is not an NbtIntArray nor an NbtList of NbtInt", nbt));
-        }
-        if (ar.length != 3) {
-            throw new AssertionError(String.format("%s has %s elements (expected 3)", nbt, ar.length));
-        }
-        return new Vec3i(ar[0], ar[1], ar[2]);
-    }
-
-    private static Vec4i readv4i(NbtElement nbt) {
-        int[] ar = null;
-        if (nbt instanceof NbtIntArray nbtia) {
-            ar = nbtia.getIntArray();
-        }
-        if (nbt instanceof NbtList nbtl) {
-            ar = new int[nbtl.size()];
-            for (int i = 0; i < nbtl.size(); ++i) {
-                ar[i] = nbtl.getInt(i);
-            }
-        }
-        if (ar == null) {
-            throw new AssertionError(String.format("%s is not an NbtIntArray nor an NbtList of NbtInt", nbt));
-        }
-        if (ar.length != 4) {
-            throw new AssertionError(String.format("%s has %s elements (expected 4)", nbt, ar.length));
-        }
-        return new Vec4i(ar[0], ar[1], ar[2], ar[3]);
     }
 }
--- src/main/java/redilog/synthesis/nodes/Component.java
+++ src/main/java/redilog/synthesis/nodes/Component.java
@@ -1,9 +1,17 @@
 package redilog.synthesis.nodes;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtHelper;
+import net.minecraft.nbt.NbtIntArray;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3i;
+import redilog.init.Redilog;
 import redilog.routing.BLOCK;
 import redilog.utils.Array3D;
 import redilog.utils.Vec4i;
@@ -12,12 +20,94 @@ import redilog.utils.Vec4i;
  * Provides details about a {@link Node}'s component, such as blocks and IO positions
  */
 public class Component {
-    //TODO margins
     public final Array3D<BLOCK> blocks;
     public final List<Vec3i> inputs = new ArrayList<>();
     public final List<Vec4i> outputs = new ArrayList<>();
+    public Vec3i margin; //minimum number of blocks between components
 
     public Component(Vec3i size) {
         this.blocks = new Array3D.Builder<BLOCK>().size(size).build();
+    }
+
+    /**
+     * load a component from nbt
+     * @param nbt data to load from, usually pulled directly from an nbt file (e.g. {@code NbtIo.readCompressed(rm.open(id));})
+     * @param id id to use in warnings
+     */
+    public Component(NbtCompound nbt, Identifier id) {
+        this(readv3i(nbt.get("size")));
+
+        for (NbtElement e : nbt.getList("inputs", NbtElement.INT_ARRAY_TYPE)) {
+            inputs.add(readv3i(e));
+        }
+        if (inputs.size() == 0) {
+            Redilog.LOGGER.warn(String.format("%s has 0 inputs", id));
+        }
+
+        for (NbtElement e : nbt.getList("outputs", NbtElement.INT_ARRAY_TYPE)) {
+            outputs.add(readv4i(e));
+        }
+        if (outputs.size() == 0) {
+            Redilog.LOGGER.warn(String.format("%s has 0 outputs", id));
+        }
+
+        margin = readv3i(nbt.get("margin"));
+
+        List<BlockState> states = new ArrayList<>();
+        for (NbtElement e : nbt.getList("palette", NbtElement.COMPOUND_TYPE)) {
+            states.add(NbtHelper.toBlockState((NbtCompound) e));
+        }
+        boolean nonAir = false;
+        for (NbtElement e : nbt.getList("blocks", NbtElement.COMPOUND_TYPE)) {
+            NbtCompound e2 = (NbtCompound) e;
+            BlockState state = states.get(e2.getInt("state"));
+            blocks.set(readv3i(e2.get("pos")), BLOCK.fromState(state));
+            if (!state.isAir()) {
+                nonAir = true;
+            }
+        }
+        if (!nonAir) {
+            Redilog.LOGGER.warn(String.format("%s has only air blocks", id));
+        }
+    }
+
+    private static Vec3i readv3i(NbtElement nbt) {
+        int[] ar = null;
+        if (nbt instanceof NbtIntArray nbtia) {
+            ar = nbtia.getIntArray();
+        }
+        if (nbt instanceof NbtList nbtl) {
+            ar = new int[nbtl.size()];
+            for (int i = 0; i < nbtl.size(); ++i) {
+                ar[i] = nbtl.getInt(i);
+            }
+        }
+        if (ar == null) {
+            throw new AssertionError(String.format("%s is not an NbtIntArray nor an NbtList of NbtInt", nbt));
+        }
+        if (ar.length != 3) {
+            throw new AssertionError(String.format("%s has %s elements (expected 3)", nbt, ar.length));
+        }
+        return new Vec3i(ar[0], ar[1], ar[2]);
+    }
+
+    private static Vec4i readv4i(NbtElement nbt) {
+        int[] ar = null;
+        if (nbt instanceof NbtIntArray nbtia) {
+            ar = nbtia.getIntArray();
+        }
+        if (nbt instanceof NbtList nbtl) {
+            ar = new int[nbtl.size()];
+            for (int i = 0; i < nbtl.size(); ++i) {
+                ar[i] = nbtl.getInt(i);
+            }
+        }
+        if (ar == null) {
+            throw new AssertionError(String.format("%s is not an NbtIntArray nor an NbtList of NbtInt", nbt));
+        }
+        if (ar.length != 4) {
+            throw new AssertionError(String.format("%s has %s elements (expected 4)", nbt, ar.length));
+        }
+        return new Vec4i(ar[0], ar[1], ar[2], ar[3]);
     }
 }
